f228ce9a66a935c378e5ceceab9fffb010cd2baa
Adolfo Reyes <adolfo0620@gmail.com>
2024-08-07 22:10:19 -0400

    adding auth for websocket

--- server/taxi/asgi.py
+++ server/taxi/asgi.py
@@ -16,11 +16,14 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'taxi.settings')
 
+from taxi.middleware import TokenAuthMiddlewareStack
 from trips.consumers import TaxiConsumer
 
 application = ProtocolTypeRouter({
     'http': get_asgi_application(),
-    'websocket': URLRouter([
-        path('taxi/', TaxiConsumer.as_asgi()),
-    ]),
+    'websocket': TokenAuthMiddlewareStack(
+        URLRouter([
+            path('taxi/', TaxiConsumer.as_asgi()),
+        ])
+    ),
 })
--- server/trips/tests/test_websocket.py
+++ server/trips/tests/test_websocket.py
@@ -4,20 +4,36 @@ from channels.testing import WebsocketCommunicator
 from taxi.asgi import application
 from channels.layers import get_channel_layer
 
+from channels.db import database_sync_to_async
+from django.contrib.auth import get_user_model
+from rest_framework_simplejwt.tokens import AccessToken
+
 TEST_CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels.layers.InMemoryChannelLayer',
     },
 }
 
+@database_sync_to_async
+def create_user(username, password):
+    user = get_user_model().objects.create_user(
+        username=username,
+        password=password
+    )
+    access = AccessToken.for_user(user)
+    return user, access
 
 @pytest.mark.asyncio
+@pytest.mark.django_db(transaction=True)
 class TestWebSocket:
     async def test_can_connect_to_server(self, settings):
         settings.CHANNEL_LAYERS = TEST_CHANNEL_LAYERS
+        _, access = await create_user(
+        'test.user@example.com', 'pAssw0rd'
+        )
         communicator = WebsocketCommunicator(
             application=application,
-            path='/taxi/'
+            path=f'/taxi/?token={access}'
         )
         connected, _ = await communicator.connect()
         assert connected is True
@@ -25,25 +41,12 @@ class TestWebSocket:
 
     async def test_can_send_and_receive_messages(self, settings):
         settings.CHANNEL_LAYERS = TEST_CHANNEL_LAYERS
-        communicator = WebsocketCommunicator(
-            application=application,
-            path='/taxi/'
+        _, access = await create_user(
+        'test.user@example.com', 'pAssw0rd'
         )
-        await communicator.connect()
-        message = {
-            'type': 'echo.message',
-            'data': 'This is a test message.',
-        }
-        await communicator.send_json_to(message)
-        response = await communicator.receive_json_from()
-        assert response == message
-        await communicator.disconnect()
-
-    async def test_can_send_and_receive_messages(self, settings):
-        settings.CHANNEL_LAYERS = TEST_CHANNEL_LAYERS
         communicator = WebsocketCommunicator(
             application=application,
-            path='/taxi/'
+            path=f'/taxi/?token={access}'
         )
         await communicator.connect()
         message = {
@@ -57,9 +60,12 @@ class TestWebSocket:
 
     async def test_can_send_and_receive_broadcast_messages(self, settings):
         settings.CHANNEL_LAYERS = TEST_CHANNEL_LAYERS
+        _, access = await create_user(
+        'test.user@example.com', 'pAssw0rd'
+        )
         communicator = WebsocketCommunicator(
             application=application,
-            path='/taxi/'
+            path=f'/taxi/?token={access}'
         )
         await communicator.connect()
         message = {
@@ -71,3 +77,14 @@ class TestWebSocket:
         response = await communicator.receive_json_from()
         assert response == message
         await communicator.disconnect()
+
+    async def test_cannot_connect_to_socket(self, settings):
+        settings.CHANNEL_LAYERS = TEST_CHANNEL_LAYERS
+        communicator = WebsocketCommunicator(
+            application=application,
+            path=f'/taxi/'
+        )
+        connected, _ = await communicator.connect()
+        assert connected is False
+        await communicator.disconnect()
+
